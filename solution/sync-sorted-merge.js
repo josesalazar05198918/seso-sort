91085086432a4a0c699cc06e3f5632f1b762c9eb
Jose Penagos <josesalazar051989@gmail.com>
2022-07-11 21:07:17 -0500

    Added sort solution
    Adding the sort solution using a external merge sort algorithm.

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,7 +1,36 @@
 "use strict";
 
-// Print all entries, across all of the sources, in chronological order.
+const {
+  cleanUp,
+  merge,
+  printResult,
+  writeToTmpFile,
+  createDir,
+} = require("./utils");
+
+const outFileName = "tmp/sync/tmp_sort_sync_result.txt";
+
+const drainLogSources = async (logSources) => {
+  const tmpFiles = [];
+  for (const logSource of logSources) {
+    tmpFiles.push(await writeToTmpFile(logSource, tmpFiles.length));
+  }
+  return tmpFiles;
+};
 
 module.exports = (logSources, printer) => {
-  return console.log("Sync sort complete.");
+  return new Promise((resolve) => {
+    createDir("tmp");
+    createDir("tmp/sync");
+
+    drainLogSources(logSources).then((tmpFiles) => {
+      merge(tmpFiles, outFileName).then(() => {
+        printResult(printer, outFileName).then(() => {
+          cleanUp(tmpFiles).then(() => {
+            resolve(console.log("Sync sort complete."));
+          });
+        });
+      });
+    });
+  });
 };
